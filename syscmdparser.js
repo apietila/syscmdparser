c242bef1c676b394b3a26bcef81660522187b246
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2014-09-01 00:17:26 +0200

    more stats

--- syscmdparser.js
+++ syscmdparser.js
@@ -180,7 +180,7 @@
 
 	case "/proc/net/dev":
 	    res.ifaces = {};
-	    for (var i = 0; i < lines.length && wifi.signal==null; i++) {
+	    for (var i = 0; i < lines.length; i++) {
 		if (lines[i].indexOf("|") >= 0 || lines[i].length <= 0)
 		    continue;
 
